aee510f75cc09dfed7975864e67343a6b765c4e4
Zeinab Abdien <suzanabdien15@gmail.com>
2023-09-06 19:10:53 +0300

    add signup fragment logic

--- app/src/main/java/com/iti/moneyapp/ui/setup/signup/SignUpFragment.kt
+++ app/src/main/java/com/iti/moneyapp/ui/setup/signup/SignUpFragment.kt
@@ -1,16 +1,27 @@
 package com.iti.moneyapp.ui.setup.signup
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.iti.moneyapp.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.iti.moneyapp.databinding.FragmentSignUpBinding
+import com.iti.moneyapp.utils.hideKeypad
+import com.iti.moneyapp.utils.validation.*
 
 class SignUpFragment : Fragment() {
 
     private lateinit var binding: FragmentSignUpBinding
+    private val viewModel: SignUpViewModel by viewModels()
+    private lateinit var fullName: String
+    private lateinit var email: String
+    private lateinit var phoneNumber: String
+    private lateinit var password: String
+    private lateinit var confirmPassword: String
+    private lateinit var error: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +29,145 @@ class SignUpFragment : Fragment() {
     ): View {
         binding = FragmentSignUpBinding.inflate(layoutInflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setOnClickListeners()
+        observation()
+    }
+
+    private fun observation() {
+        viewModel.email.observe(viewLifecycleOwner) { emailError ->
+            if (!emailError.toString().equals(null))
+                error = emailError.toString()
+        }
+
+        viewModel.fullName.observe(viewLifecycleOwner) { fullNameError ->
+            if (!fullNameError.toString().equals(null))
+                error = fullNameError.toString()
+
+        }
+    }
+
+    private fun setOnClickListeners() {
+        binding.apply {
+            btnBack.setOnClickListener {
+                findNavController().navigateUp()
+            }
+            btnSignup.setOnClickListener {
+                requireActivity().hideKeypad()
+                userData()
+                val isValidSignUpData = viewModel.isValidSignUp(
+                    email = email,
+                    fullName = fullName,
+                    phone = phoneNumber,
+                    password = password,
+                    confirmPassword = confirmPassword
+                )
+                if (isValidSignUpData) {
+                    if (viewModel.signIn(email, password)) {
+                        Toast.makeText(requireContext(), "Account Created", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            "Account is Using this email",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                } else {
+                    showError()
+                    viewModel.resetData()
+                }
+            }
+        }
+    }
+
+
+    private fun userData() {
+        binding.apply {
+            fullName = etName.text.toString()
+            email = etEmail.text.toString()
+            phoneNumber = etPhone.text.toString()
+            password = etPassword.text.toString()
+            confirmPassword = etConfirmPassword.text.toString()
+        }
+    }
+
+    private fun showError() {
+
+        if (viewModel.fullName.value.toString() != "null")
+            Toast.makeText(
+                context,
+                fullNameError(viewModel.fullName.value),
+                Toast.LENGTH_LONG
+            ).show()
+        else if (viewModel.email.value.toString() != "null")
+            Toast.makeText(
+                context,
+                emailError(viewModel.email.value),
+                Toast.LENGTH_LONG
+            ).show()
+        else if (viewModel.phoneNumber.value.toString() != "null")
+            Toast.makeText(
+                context,
+                phoneNumber(viewModel.phoneNumber.value),
+                Toast.LENGTH_LONG
+            ).show()
+        else if (viewModel.password.value.toString() != "null")
+            Toast.makeText(
+                context,
+                passwordError(viewModel.password.value),
+                Toast.LENGTH_LONG
+            ).show()
+        else if (viewModel.confirmPassword.value.toString() != "null")
+            Toast.makeText(
+                context,
+                confirmPasswordError(viewModel.confirmPassword.value),
+                Toast.LENGTH_LONG
+            ).show()
+
+    }
+
+    private fun phoneNumber(phoneError: PhoneError?): String {
+        return when (phoneError) {
+            PhoneError.INVALID -> "Invalid phone number"
+            else -> "Phone number field is empty"
+        }
+    }
+
+    private fun emailError(emailError: EmailError?): String {
+        return when (emailError) {
+            EmailError.INVALID -> "Invalid Email"
+            else -> "Email field is empty"
+        }
+
+    }
+
+    private fun passwordError(passwordError: PasswordError?): String {
+        return when (passwordError) {
+            PasswordError.IS_LONG -> "Password is too long"
+            PasswordError.IS_SHORT -> "Password is too short"
+            else -> "Password field is empty"
+        }
+    }
+
+    private fun confirmPasswordError(confirmPasswordError: ConfirmPasswordError?): String {
+        return when (confirmPasswordError) {
+            ConfirmPasswordError.NOT_MATCHING_PASSWORD -> "Confirm password doesn't match"
+            else -> "Confirm password field is empty"
+        }
+    }
+
+    private fun fullNameError(fullNameError: FullNameError?): String {
+        return when (fullNameError) {
+            FullNameError.INVALID -> "Invalid name"
+            FullNameError.IS_SHORT -> "Name is short"
+            FullNameError.IS_LONG -> "Name is long"
+            else -> "Full name field is empty"
+        }
     }
 }
