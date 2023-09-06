b7ef663839b9a6ffb73ac03f1f8bf990da00f2d8
Zeinab Abdien <suzanabdien15@gmail.com>
2023-09-06 19:11:53 +0300

    setup repository logic for login and signup

---
+++ app/src/main/java/com/iti/moneyapp/ui/setup/SetupRepository.kt
@@ -0,0 +1,20 @@
+package com.iti.moneyapp.ui.setup
+
+import com.google.firebase.auth.FirebaseAuth
+
+class SetupRepository {
+
+    private fun getAuthFirebase() = FirebaseAuth.getInstance()
+
+    fun signWithEmail(email: String, password: String) =
+        getAuthFirebase().createUserWithEmailAndPassword(email, password).isSuccessful
+
+
+    fun loginWithEmail(email: String, password: String) =
+        getAuthFirebase().signInWithEmailAndPassword(email, password).isSuccessful
+
+    fun forgotPassword(email: String) {
+        getAuthFirebase().sendPasswordResetEmail(email)
+    }
+}
+
