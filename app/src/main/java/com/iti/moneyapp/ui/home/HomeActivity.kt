40248e354033aa40a595edad1be9fc0e081a9c8d
Zeinab Abdien <suzanabdien15@gmail.com>
2023-09-19 22:45:45 +0300

    test tab design on activity

--- app/src/main/java/com/iti/moneyapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/iti/moneyapp/ui/home/HomeActivity.kt
@@ -1,12 +1,55 @@
 package com.iti.moneyapp.ui.home
 
+import android.graphics.drawable.Icon
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHostController
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.iti.moneyapp.R
+import com.iti.moneyapp.adapter.home.TapViewPagerAdapter
+import com.iti.moneyapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var adapter: TapViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = TapViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Income"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Expense"))
+
+        binding.viewPager.adapter = adapter
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null)
+                    binding.viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+
+            }
+        })
+
+        binding.viewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tabLayout.selectTab(binding.tabLayout.getTabAt(position))
+            }
+        })
     }
 }
--- app/src/main/java/com/iti/moneyapp/ui/home/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/iti/moneyapp/ui/home/dashboard/DashboardFragment.kt
@@ -1,20 +1,55 @@
 package com.iti.moneyapp.ui.home.dashboard
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.iti.moneyapp.R
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.iti.moneyapp.adapter.home.TapViewPagerAdapter
+import com.iti.moneyapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var binding: FragmentDashboardBinding
+    private lateinit var adapter: TapViewPagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    ): View {
+        binding = FragmentDashboardBinding.inflate(layoutInflater)
+
+        adapter = TapViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Details"))
+        binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Community"))
+
+        binding.viewPager.adapter = adapter
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null)
+                    binding.viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+
+        binding.viewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                binding.tabLayout.selectTab(binding.tabLayout.getTabAt(position))
+            }
+        })
+
+        return binding.root
     }
 
 }
